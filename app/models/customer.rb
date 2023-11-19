2555e57d1ddd42fda70797244296d31816ccac9b
agamstawn <setiawanagam@gmail.com>
2023-11-20 01:53:53 +0700

    [major updates]   information about a customer   -  add current tier info   -  fix update tier in customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,7 +14,7 @@ class Customer < ApplicationRecord
   before_save :set_init_tier
   
   def update_tier
-    total_spent = orders.sum(:totalInCents)
+    total_spent = orders.where(date: Date.today.beginning_of_year.prev_year..Date.today.end_of_year.prev_year).sum(:totalInCents)
 
     if total_spent < 100
       new_tier = "Bronze"
@@ -24,10 +24,79 @@ class Customer < ApplicationRecord
       new_tier = "Gold"
     end
 
-    update(tier: "Tier #{new_tier}")
+    update(tier: new_tier)
   end
 
   def set_init_tier
-    self.tier = "Bronze" if self.tier.blank?
+    self.tier = "Bronze" if self.tier.blank? 
+  end
+
+  def current_tier_info
+    current_tier = tier
+    start_date = Date.today.beginning_of_year.prev_year
+    amount_spent_since_start_date = calculate_amount_spent_since(start_date)
+    next_tier_amount = calculate_next_tier_amount
+    downgrade_date = Date.today.end_of_year
+    downgrade_next_year = calculate_downgrade_next_year
+    keep_current_tier_amount = calculate_keep_current_tier_amount
+
+    {
+      current_tier: current_tier,
+      start_date: start_date,
+      amount_spent_since_start_date: amount_spent_since_start_date,
+      next_tier_amount: next_tier_amount,
+      downgrade_date: downgrade_date,
+      # amount_this_year: calculate_amount_spent_this_year,
+      downgrade_next_year: downgrade_next_year,
+      keep_current_tier_amount: keep_current_tier_amount
+    }
+  end
+
+  private
+
+  def calculate_amount_spent_since(start_date)
+    orders.where('date >= ?', start_date).sum(:totalInCents)
+  end
+
+  def calculate_amount_spent_this_year
+    orders.where(date: Date.today.beginning_of_year..Date.today.end_of_year).sum(:totalInCents)
+  end
+
+  def calculate_next_tier_amount
+    case 
+    when tier.eql?("Bronze")
+      next_tier_amount = 100 - calculate_amount_spent_this_year
+    when tier.eql?("Silver")
+      next_tier_amount = 500 - calculate_amount_spent_this_year
+    when tier.eql?("Gold")
+      next_tier_amount = 0
+    end
+    
+    next_tier_amount
+  end
+  
+  def calculate_downgrade_next_year
+    case 
+    when tier.eql?("Bronze") || tier.eql?("Silver")
+      downgrade_tier = "Bronze"
+    when tier.eql?("Gold")
+      downgrade_tier = "Silver"
+    end
+
+    downgrade_tier
+  end
+
+  def calculate_keep_current_tier_amount
+    case 
+    when tier.eql?("Bronze")
+      keep_amount = 0  
+    when tier.eql?("Silver")
+      keep_amount = 100 - calculate_amount_spent_this_year
+    when tier.eql?("Gold")
+      keep_amount = 500 - calculate_amount_spent_this_year
+    end
+
+    keep_amount = 0 if keep_amount <= 0
+    keep_amount 
   end
 end
