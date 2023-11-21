cd95562be1e55940c7387bee52742edaa2299916
agamstawn <setiawanagam@gmail.com>
2023-11-22 00:26:17 +0700

    [minor updates] :   -  add rack cors to gem   -  add Rack Cors config to application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module LoyaltyTiersApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Add the origin of your React app
+        resource '/api/*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+    
   end
 end
